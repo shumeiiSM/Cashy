ca2815938af7128a25faef2994e16fd93ec22ed2
17010233 <17010233@myrp.edu.sg>
2019-05-28 03:08:26 +0800

    floating

---
+++ app/src/main/java/com/example/a17010233/cashy/AddOweBill.java
@@ -0,0 +1,14 @@
+package com.example.a17010233.cashy;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class AddOweBill  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_owe_bill);
+    }
+}
+
--- app/src/main/java/com/example/a17010233/cashy/HomeFragment.java
+++ app/src/main/java/com/example/a17010233/cashy/HomeFragment.java
@@ -6,10 +6,11 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 
 
@@ -19,6 +20,9 @@ import android.widget.Button;
 public class HomeFragment extends Fragment {
 
     Button btnYouOwe, btnOweYou;
+    FloatingActionButton fab_add, fab_minus, fab;
+    Animation FabOpen, FabClose, FabClock, FabAntiClock;
+    boolean isOpen = false;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -34,9 +38,43 @@ public class HomeFragment extends Fragment {
         View rootView = getActivity().findViewById(android.R.id.content);
         Snackbar.make(rootView, "Hello Home", Snackbar.LENGTH_LONG).show();
 
-        FloatingActionButton mFab = view.findViewById(R.id.fab);
+        btnYouOwe = view.findViewById(R.id.btnB);
+        btnOweYou = view.findViewById(R.id.btnO);
 
-        mFab.setOnClickListener(new View.OnClickListener() {
+        fab = view.findViewById(R.id.fab);
+        fab_add = view.findViewById(R.id.fab_add);
+        fab_minus = view.findViewById(R.id.fab_minus);
+
+        FabOpen = AnimationUtils.loadAnimation(getActivity(), R.anim.fab_open);
+        FabClose = AnimationUtils.loadAnimation(getActivity(), R.anim.fab_close);
+        FabClock = AnimationUtils.loadAnimation(getActivity(), R.anim.rotate_closewise);
+        FabAntiClock = AnimationUtils.loadAnimation(getActivity(), R.anim.rotate_anticlockwise);
+
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fab.setImageResource(R.drawable.ic_close);
+                if (isOpen) {
+                    fab_add.startAnimation(FabClose);
+                    fab_minus.startAnimation(FabClose);
+                    fab.startAnimation(FabAntiClock);
+                    fab_add.setClickable(false);
+                    fab_minus.setClickable(false);
+                    isOpen = false;
+
+                } else {
+                    fab_add.startAnimation(FabOpen);
+                    fab_minus.startAnimation(FabOpen);
+                    fab.startAnimation(FabClock);
+                    fab_add.setClickable(true);
+                    fab_minus.setClickable(true);
+                    isOpen = true;
+                }
+            }
+        });
+
+        fab_add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getActivity(), AddBill.class);
@@ -44,9 +82,29 @@ public class HomeFragment extends Fragment {
             }
         });
 
+        fab_minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), AddBill.class);
+                startActivity(intent);
+            }
+        });
 
+        btnYouOwe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), YouOwe.class);
+                startActivity(intent);
+            }
+        });
 
-        //View v = getActivity().findViewById(R.id.btnO);
+        btnOweYou.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), OweYou.class);
+                startActivity(intent);
+            }
+        });
 
 
 
